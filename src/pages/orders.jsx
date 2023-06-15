a547de901eedc8f67f9a1b0d31442d186fcfca53
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-06-16 01:08:40 +0600

    orders route

--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -1,5 +1,64 @@
+import { FaShoppingBag } from "react-icons/fa";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { data } from "../data/data.js";
+
 const orders = () => {
-  return <div>orders</div>;
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <div className="flex p-4 justify-between">
+        <h1>Orders</h1>
+        <h1>Welcome back, Client!</h1>
+      </div>
+      <div className="p-4">
+        <div className="w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto">
+          <div className="my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer">
+            <span>Order</span>
+            <span className="text-right sm:text-left">Status</span>
+            <span className="hidden md:grid">Last Order</span>
+            <span className="hidden sm:grid">Method</span>
+          </div>
+          <ul>
+            {data.map((order, i) => (
+              <li
+                key={i}
+                className="bg-gray-50 hover:bg-gray-100 overflow-y-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 items-center cursor-pointer justify-between rounded-lg my-3 p-2"
+              >
+                <div className="flex items-center">
+                  <div className="bg-purple-100 p-3 rounded-lg">
+                    <FaShoppingBag className="text-purple-800" />
+                  </div>
+                  <div className="pl-4">
+                    <p className=" text-gray-800 font-bold">
+                      {order.total.toLocaleString()}
+                    </p>
+                    <p className="text-gray-800 text-sm">{order.name.first}</p>
+                  </div>
+                </div>
+                <p className="text-gray-600 sm:text-left text-right">
+                  <span
+                    className={
+                      order.status == "Processing"
+                        ? "bg-green-200 p-2 rounded-lg"
+                        : order.status == "Completed"
+                        ? "bg-blue-200 p-2 rounded-lg"
+                        : "bg-yellow-200 p-2 rounded-lg"
+                    }
+                  >
+                    {order.status}
+                  </span>
+                </p>
+                <p className="hidden md:flex">{order.date}</p>
+                <div className="sm:flex hidden justify-between items-center">
+                  <p>{order.method}</p>
+                  <BsThreeDotsVertical />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default orders;
